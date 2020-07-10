7b16c80d00f1b72841b10c84fd1a95f0bc5bbde6
yaminyassin <Yamin2008@live.com>
2020-07-10 01:02:00 +0100

    comecei promos page

--- src/app/pages/promos/promos.page.ts
+++ src/app/pages/promos/promos.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-promos',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promos.page.scss'],
 })
 export class PromosPage implements OnInit {
+  lat:number;
+  lng:number;
+  notifs:any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.notifs = [];
+  }
+  
+
+  async loadData(){
+    try {
+      const position = await Geolocation.getCurrentPosition({
+        enableHighAccuracy: true,
+        timeout: 1000
+      });
+
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+     } catch(err) {
+        console.log(err);
+    }
+
+    //CONTINUAR AQUI (TENHO DE CRIAR
+    // A QUERY PARA ESTA PAGINA)
   }
 
 }
